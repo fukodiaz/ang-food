fb6f31e2d6a5cc214ce902aafb27b8c81a0fc28c
Fuko <fukodiaz@gmail.com>
2023-04-17 22:13:45 +0700

    add test item in list-recipe directly

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,9 +1,12 @@
 import { Component } from "@angular/core";
+import { Recipe } from '../recipe.model'
 
 @Component({
 	selector: 'app-recipe-list',
 	templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent {
-	recipes = []
+	recipes: Recipe[] = [
+		new Recipe('First Recipe', 'Some description of this recipe', 'https://images.unsplash.com/photo-1518779578993-ec3579fee39f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80')
+	]
 }
