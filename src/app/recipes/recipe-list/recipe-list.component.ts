24b4a51d6d57b36e5dc7844e0d972cae07dc0984
Fuko <fukodiaz@gmail.com>
2023-04-19 17:48:18 +0700

    isolate template for item-recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,6 +7,7 @@ import { Recipe } from '../recipe.model'
 })
 export class RecipeListComponent {
 	recipes: Recipe[] = [
-		new Recipe('First Recipe', 'Some description of this recipe', 'https://images.unsplash.com/photo-1518779578993-ec3579fee39f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80')
+		new Recipe('First Recipe', 'Some description of this recipe', 'https://images.unsplash.com/photo-1518779578993-ec3579fee39f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80'),
+		new Recipe('Second Recipe', 'Some description of this recipe', 'https://images.unsplash.com/photo-1518779578993-ec3579fee39f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80')
 	]
 }
