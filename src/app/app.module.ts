6df0c56444f3c7b6a22395374befdd4361ab2471
Fuko <fukodiaz@gmail.com>
2023-05-17 21:55:56 +0700

    use interceptor for attaching token

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http'
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -20,6 +20,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
 import { RecipeService } from './recipes/recipe.service';
 import { AuthComponent } from './auth/auth.component';
 import { SpinnerLoadingComponent } from './shared/spinner-loading/spinner-loading.component';
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
 
 @NgModule({
 	declarations: [
@@ -45,7 +46,15 @@ import { SpinnerLoadingComponent } from './shared/spinner-loading/spinner-loadin
 		ReactiveFormsModule,
 		HttpClientModule
 	],
-	providers: [ShoppingListService, RecipeService],
+	providers: [
+		ShoppingListService, 
+		RecipeService,
+		{
+			provide: HTTP_INTERCEPTORS,
+			useClass: AuthInterceptorService,
+			multi: true
+		}
+	],
 	bootstrap: [AppComponent]
 })
 export class AppModule { }
