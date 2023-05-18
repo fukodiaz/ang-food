e71989b5ebf75e84082107d4e1c856d363052ea3
Fuko <fukodiaz@gmail.com>
2023-05-18 23:16:59 +0700

    add autologout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -18,6 +18,7 @@ export interface AuthResponseData {
 @Injectable({providedIn: 'root'})
 export class AuthService {
 	user = new BehaviorSubject<User | null>(null)
+	tokenExpirationTimer: any
 
 	constructor(private http: HttpClient,
 					private router: Router) {}
@@ -73,12 +74,25 @@ export class AuthService {
 
 		if (loadedUser.token) {
 			this.user.next(loadedUser)
+			const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime()
+			this.autoLogout(expirationDuration)
 		}
 	}
 
 	logout() {
 		this.user.next(null)
+		localStorage.removeItem('userData')
 		this.router.navigate(['/auth'])
+		if (this.tokenExpirationTimer) {
+			clearTimeout(this.tokenExpirationTimer)
+		}
+		this.tokenExpirationTimer = null
+	}
+
+	autoLogout(expirationDuration: number) {
+		this.tokenExpirationTimer = setTimeout(() => {
+			this.logout()
+		}, -2000)
 	}
 
 	private handleAuthentication(
@@ -89,6 +103,7 @@ export class AuthService {
 		const expirationDate = new Date(new Date().getTime() + expiresIn*1000)
 		const user = new User(localId, email, idToken, expirationDate)
 		this.user.next(user)
+		this.autoLogout(expiresIn*1000)
 		localStorage.setItem('userData', JSON.stringify(user))
 	}
 
